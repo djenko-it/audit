5a0db5f25a5b45a408268ed325f951b16c6adf97
djenko-it <djenko-it@protonmail.com>
2024-06-15 21:23:25 +0200

    mac

--- app/scanner.py
+++ app/scanner.py
@@ -2,15 +2,24 @@ import nmap
 
 def scan_network(ip_range, ports):
     nm = nmap.PortScanner()
-    nm.scan(ip_range, ports)
+    nm.scan(ip_range, ports, arguments='-O')
+
     results = []
     for host in nm.all_hosts():
         host_info = {
             'host': host,
             'hostname': nm[host].hostname(),
             'state': nm[host].state(),
+            'mac': None,
+            'vendor': None,
             'ports': []
         }
+        
+        if 'mac' in nm[host]['addresses']:
+            host_info['mac'] = nm[host]['addresses']['mac']
+        if 'vendor' in nm[host]:
+            host_info['vendor'] = nm[host]['vendor']
+
         for proto in nm[host].all_protocols():
             lport = nm[host][proto].keys()
             for port in lport:
